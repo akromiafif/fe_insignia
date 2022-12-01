59c6d0051cb8d3eac9a175fda90539988a451428
Afif Akromi <massachusetts48@gmail.com>
2022-12-01 14:43:54 +0700

    init project

---
+++ components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="flex w-full flex-row">
+      <p className="text-4xl  text-[#e5b8b0]">
+        Social<span className="text-[#c5817f]">Network</span>
+      </p>
+    </div>
+  );
+};
+
+export default Header;
