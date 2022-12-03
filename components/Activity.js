823b42ba9e6a9bf518f73d7d60d1daee83fa091f
Afif Akromi <massachusetts48@gmail.com>
2022-12-03 23:24:37 +0700

    fix Activity with hover

--- components/Activity.js
+++ components/Activity.js
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import { RxSlash } from "react-icons/rx";
 import { FiSearch, FiUpload } from "react-icons/fi";
+import { IoMdClose } from "react-icons/io";
 import { BiCommentDetail } from "react-icons/bi";
 
 const Activity = () => {
+  const [isHover, setIsHover] = useState(false);
+  const [selected, setSelected] = useState(-1);
+
   const activites = [
     {
       name: "John Stainor",
@@ -47,19 +51,43 @@ const Activity = () => {
       </div>
       <div className="border-2 border-x-0 border-t-0 bg-secondary"></div>
       <div className="flex flex-col space-y-2 cursor-pointer ">
-        {activites.map((item) => {
+        {activites.map((item, index) => {
           return (
-            <div className="flex flex-row items-center space-x-4 hover:border-2 hover:border-red-100">
-              <div className="bg-secondary w-20 h-full"></div>
-              <div className="flex flex-col w-3/4">
-                <p>
-                  <span className="text-secondary text-lg">{item.name}</span>
-                  <span className="text-[#c5817f]"> {item.status}</span>
-                </p>
-                <p className="text-secondary leading-[18px]">{item.title}</p>
-                <div className="flex flex-row space-x-2 items-center">
-                  {item.icon}
-                  <p className="text-secondary">{item.time}</p>
+            <div
+              className="relative"
+              onMouseOver={() => {
+                setIsHover(true);
+                setSelected(index);
+              }}
+              onMouseLeave={() => setIsHover(false)}
+            >
+              {isHover && index == selected && (
+                <div
+                  style={{ "clip-path": "polygon(0 0, 100% 100%, 100% 0)" }}
+                  className="absolute right-0 top-0 h-8 w-8 bg-secondary z-10"
+                >
+                  <IoMdClose
+                    color="#953e45"
+                    className="right-0 top-[3px] absolute"
+                  />
+                </div>
+              )}
+
+              <div className="flex flex-row items-center space-x-4 hover:border-2 hover:border-red-100">
+                <div className="bg-secondary w-24 h-24 -z-0"></div>
+                <div className="flex flex-col w-3/4 h-20 -z-0">
+                  <p>
+                    <span className="text-secondary text-lg">{item.name}</span>
+                    <span className="text-[#c5817f] text-sm">
+                      {" "}
+                      {item.status}
+                    </span>
+                  </p>
+                  <p className="text-secondary leading-[18px]">{item.title}</p>
+                  <div className="flex flex-row space-x-2 items-center">
+                    {item.icon}
+                    <p className="text-secondary">{item.time}</p>
+                  </div>
                 </div>
               </div>
             </div>
