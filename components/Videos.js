ae4e734d6e468f5e5cfc1acda2a5310042672a84
Afif Akromi <massachusetts48@gmail.com>
2022-12-03 21:53:37 +0700

    fix Videos component

--- components/Videos.js
+++ components/Videos.js
@@ -2,8 +2,37 @@ import React from "react";
 
 const Videos = () => {
   return (
-    <div className="w-8/12 flex flex-row">
-      <div className="flex flex-col"></div>
+    <div className="w-8/12 flex flex-row space-x-4">
+      <div className="flex flex-col w-8/12 space-y-2 space-y-4">
+        <div className="flex flex-row items-end justify-between">
+          <p className="text-secondary text-3xl font-bold">Videos</p>
+          <p className="text-secondary">Browse all videos</p>
+        </div>
+        <div className="w-full h-80 bg-secondary flex flex-col  px-4 py-4 justify-end">
+          <p className="text-primary text-xl font-bold">
+            How to improve your skills
+          </p>
+          <div className="flex flex-row justify-between">
+            <p className="text-primary text-lg leading-3">John Stainor</p>
+            <p className="text-primary text-lg leading-3">5435765 Views</p>
+          </div>
+        </div>
+        <div className="flex flex-row space-x-4">
+          <div className="w-1/2 h-60 bg-secondary flex flex-col px-4 py-4 justify-end">
+            <div className="flex flex-row justify-between">
+              <p className="text-primary leading-3">Hasheem Ali</p>
+              <p className="text-primary leading-3">3451 Views</p>
+            </div>
+          </div>
+          <div className="w-1/2 h-60 bg-secondary flex flex-col px-4 py-4 justify-end">
+            <div className="flex flex-row justify-between">
+              <p className="text-primary leading-3">Lutfi Wira</p>
+              <p className="text-primary leading-3">4567 Views</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="flex flex-col bg-secondary w-4/12"></div>
     </div>
   );
 };
--- pages/index.js
+++ pages/index.js
@@ -5,10 +5,10 @@ import Videos from "../components/Videos";
 
 export default function Home() {
   return (
-    <div className="flex space-y-14 h-screen w-full flex-col bg-[#953e45] px-44 py-10">
+    <div className="flex space-y-14 w-full flex-col bg-[#953e45] px-44 py-10">
       <Header />
       <BreadCrumbs />
-      <div className="w-full flex flex-row">
+      <div className="w-full flex flex-row space-x-4">
         <Videos />
         <Activity />
       </div>
